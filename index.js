7d9498b3b8a0bec2384518fbf55972ecf5f7136b
Leandro Rial <Leandro_Rial@outloo.com>
2021-03-25 15:07:41 -0300

    make it better 2

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ app.put("/api/update", (req, res) => {
 })
 
 // Server
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log("Server running on PORT 5000 ");
+app.listen(PORT, () => {
+  console.log("Server running on PORT: ", PORT);
 });
